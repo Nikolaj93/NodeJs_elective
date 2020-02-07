877ea4aa065a05247b18e18bfc2f921ac81f6561
Nikolaj93 <nikolaj.hansen93@gmail.com>
2020-02-07 10:47:31 +0100

    initial commit

---
+++ 2/1._Intro_Files/firstFile.js
@@ -0,0 +1,12 @@
+// + changes datatypes - therefore use commas instead!
+console.log("hello", "world");
+
+var name = "Nikolaj";
+
+console.log("Hello", name);
+
+var age = 26
+
+console.log(name, "is", age, "years old")
+
+var anotherName = "Mr. Hansen"
