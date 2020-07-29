d0252dccc00fbd036e015a9d5ccd4c284c6d06a7
Nikolaj93 <nikolaj.hansen93@gmail.com>
2020-07-29 05:07:39 +0200

    fs-extra added plus other stuff

--- 2/Mandatory_2/app.js
+++ 2/Mandatory_2/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const fs = require('fs-extra');
 const app = express();
 
 app.use(express.json());
 
+app.use(express.static('frontend'));
+
 const session = require('express-session');
 app.use(session({
-    secret: require('./config/mysqlCredentials.js').sessionSecret,
+    secret: require('./config/mysqlCredentials.js').development.session, //sessionSecret?
     resave: false,
     saveUninitialized: true
 }));
@@ -13,23 +16,36 @@ app.use(session({
 const rateLimit = require("express-rate-limit");
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 8 // limit each IP to 100 requests per windowMs
+    max: 8
 });
 app.use("/login", limiter);
 app.use("/signup", limiter);
 
 
-/* Setup Objection + Knex */
+// Setup Objection + Knex
 
 const { Model } = require('objection');
 const Knex = require('knex');
 const knexFile = require('./knexfile.js');
-
 const knex = Knex(knexFile.development);
-
 Model.knex(knex);
 
-/* Add routes */
+// Pages
+
+const headerPage = fs.readFileSync(__dirname + "/frontend/header/header.html", "utf8");
+const footerPage = fs.readFileSync(__dirname + "/frontend/footer/footer.html", "utf8");
+
+//Home pages
+
+const adminPage = fs.readFileSync(__dirname + "/frontend/frontpages/homeAdmin.html", "utf8");
+const userPage = fs.readFileSync(__dirname + "/frontend/frontpages/homeUser.html", "utf8");
+
+//Content pages
+
+const noAccess = fs.readFileSync(__dirname + "/frontend/contentPages/noAcces.html", "utf8");
+const secretPage = fs.readFileSync(__dirname + "/frontend/contentPages/secret.html", "utf8");
+
+// Add routes
 
 const authRoute = require('./routes/auth.js');
 const usersRoute = require('./routes/users.js');
@@ -37,10 +53,7 @@ const usersRoute = require('./routes/users.js');
 app.use(authRoute);
 app.use(usersRoute);
 
-/* Start server */
-
-
-
+// Start server
 
 const PORT = 3000;
 
